708766b5139f264d7e16b1647a37d350bd7db0e0
quietdw <1044569102@qq.com>
2019-03-20 11:19:42 +0800

    将svg整理到 icon.vue

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
 
 Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
 
 new Vue({
-  el:'#app',
-  data:{
-    message:'按钮'
-  }
+  el:'#app'
 })
