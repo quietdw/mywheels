b6a000f3295e9222471a34ba39ae85cadc5ce49c
quietdw <1044569102@qq.com>
2019-03-21 09:28:44 +0800

    通过$emit给组件添加点击事件

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" v-on:click="$emit('click')">
     <g-icon v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon v-if="loading" class="loading" name="loading"></g-icon>
     <slot></slot>
@@ -8,55 +8,57 @@
 
 <script>
 export default {
-  props:{
-    icon:{},
-    loading:{
-      type:Boolean,
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
       default: false
     },
-    iconPosition:{ // key 是传来的属性名字，value是该属性的配置信息
-      type:String,
-      default: 'left',
-      validator:function(value){  // 属性检查器
-        return value ==='left' || value === 'right'
+    iconPosition: {
+      // key 是传来的属性名字，value是该属性的配置信息
+      type: String,
+      default: "left",
+      validator: function(value) {
+        // 属性检查器
+        return value === "left" || value === "right";
       }
     }
-  },
-}
+  }
+};
 </script>
 
 <style lang="scss">
-    .g-button{
-      font-size: var(--font-size);
-      height: var(--button-height);
-      padding: 0 .85em;
-      border-radius: var(--border-radius);
-      border: 1px solid var(--border-color);
-      background: var(--botton-bg);
-      color: var(--color);
-      cursor: pointer;
-      display: inline-flex;
-      vertical-align: top;
-      justify-content: center;
-      align-items: center;
-      &:hover{
-       border-color: var(--border-hover-color);
-      }
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 0.85em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--botton-bg);
+  color: var(--color);
+  cursor: pointer;
+  display: inline-flex;
+  vertical-align: top;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    border-color: var(--border-hover-color);
+  }
 
-      &:active{
-       background-color: var(--button-active-bg);
-      }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
 
-      &:focus{
-       outline: none;
-      }
-      &.icon-right>.g-icon{
-        order:1;
-        margin-left: .3em;
-      }
-      &.icon-left>.g-icon{
-        margin-right: .3em;
-      }
-    }
+  &:focus {
+    outline: none;
+  }
+  &.icon-right > .g-icon {
+    order: 1;
+    margin-left: 0.3em;
+  }
+  &.icon-left > .g-icon {
+    margin-right: 0.3em;
+  }
+}
 </style>
 
