d1c108a62a7d8d3129c031d58790894a02ad26a1
quietdw <1044569102@qq.com>
2019-03-21 11:38:26 +0800

    使用chai 单元测试

--- src/app.js
+++ src/app.js
@@ -10,6 +10,78 @@ Vue.component('g-button-group', ButtonGroup)
 new Vue({
   el: '#app',
   data: {
-    loading1: false
+    loading1: false,
   }
 })
+
+// 单元测试  button有几个props属性就车市几个 外加一个点击事件
+import chai from 'chai'
+const expect = chai.expect;
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  const use = vm.$el.querySelector('use')
+  expect(use.getAttribute('xlink:href')).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true,
+    }
+  })
+  vm.$mount()
+  const use = vm.$el.querySelector('use')
+  expect(use.getAttribute('xlink:href')).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: false,
+      iconPosition: "right"
+    }
+  })
+  vm.$mount(div)
+  const svg = vm.$el.querySelector('svg')
+  let {
+    order
+  } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    console.log(1)
+  })
+
+  vm.$el.click()
+  vm.$el.remove()
+  vm.$destroy()
+}
