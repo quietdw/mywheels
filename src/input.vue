2ae921f2c2a320f8bfdd4c148329af443adcdf15
quietdw <1044569102@qq.com>
2019-03-24 17:54:12 +0800

    绑定v-model

--- src/input.vue
+++ src/input.vue
@@ -5,10 +5,10 @@
       v-bind:value="value"
       v-bind:disabled="disabled"
       :readonly="readonly"
-      @change="$emit('change',$event)"
-      @input="$emit('input',$event)"
-      @focus="$emit('focus',$event)"
-      @blur="$emit('blur',$event)"
+      @change="$emit('change',$event.target.value)"
+      @input="$emit('input',$event.target.value)"
+      @focus="$emit('focus',$event.target.value)"
+      @blur="$emit('blur',$event.target.value)"
     >
     <template v-if="error">
       <icon name="error" class="icon-error"></icon>
@@ -16,6 +16,30 @@
     </template>
   </div>
 </template>
+
+<script>
+import Icon from "./icon";
+export default {
+  components: { Icon },
+  name: "my-g-input",
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
+};
+</script>
 
  <style lang="scss" scoped>
 //       --button-height: 32px;
@@ -75,27 +99,3 @@ $red: #f1453d;
   }
 }
 </style>
-
-<script>
-import Icon from "./icon";
-export default {
-  components: { Icon },
-  name: "my-g-input",
-  props: {
-    value: {
-      type: String
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    readonly: {
-      type: Boolean,
-      default: false
-    },
-    error: {
-      type: String
-    }
-  }
-};
-</script>
