0ade1d39511e41374a5d59f9bc2423618ce903e1
quietdw <1044569102@qq.com>
2019-03-25 01:43:26 +0800

    row 和 col 的雏形

---
+++ src/col.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-g-col"
+};
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: aquamarine;
+  border: solid 1px black;
+  width: 50%;
+
+  $class: col-;
+
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: ($n/24) * 100%;
+    }
+  }
+}
+</style>
+
---
+++ src/row.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-g-row"
+};
+</script>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+}
+</style>
+
