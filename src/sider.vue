4e89c2fa490941df371eade17abf08e1e119cf4d
quietdw <1044569102@qq.com>
2019-03-28 00:39:46 +0800

    sider 添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="hide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="hideSider">close</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
-  name: "MySider"
+  name: "MySider",
+  data() {
+    return {
+      visible: true
+    };
+  },
+  methods: {
+    hideSider() {
+      this.visible = false;
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
+.hide-leave-active {
+  transition: all 1s ease;
+}
+.hide-enter,
+.hide-leave-to {
+  margin-left: -73px;
+  opacity: 0;
+}
 </style>
