014f3c25e4af389e6fa852d8b25e8f172460c221
quietdw <1044569102@qq.com>
2019-03-28 11:44:58 +0800

    简单实现 toast 组件，涉及动态添加组件。

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Footer from './footer'
 import Content from './content'
 import Layout from './layout'
 import Sider from './sider'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,9 @@ Vue.component('g-footer', Footer)
 Vue.component('g-content', Content)
 Vue.component('g-layout', Layout)
 Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
+
+Vue.use(plugin)
 
 new Vue({
   el: '#app',
@@ -29,9 +34,12 @@ new Vue({
     loading1: false,
     message: "hi"
   },
+  created() {
+
+  },
   methods: {
-    inputChange(e) {
-      console.log(e.target.value)
+    xxx() {
+      this.$toast('xxxx')
     }
   }
 })
---
+++ src/toast.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyToast"
+};
+</script>
+<style lang="scss" scoped>
+.toast {
+  position: absolute;
+  left: 50%;
+  top: 0;
+  transform: translateX(-50%);
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+  background: #a94442;
+  color: #fff;
+  border-radius: 0 0 4px 4px;
+  font-size: 16px;
+}
+</style>
