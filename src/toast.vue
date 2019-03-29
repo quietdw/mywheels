297a495112f8c3df43642a5899e6240c903d6017
quietdw <1044569102@qq.com>
2019-03-29 09:27:16 +0800

    toast 增加进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="toast" ref="wapper" :class="toastClass">
-    <div class="message">
-      <slot v-if="!closeButton.enableHTML"></slot>
-      <div v-else v-html="this.$slots.default[0]"></div>
+  <div class="wapper" :class="toastClass">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!closeButton.enableHTML"></slot>
+        <div v-else v-html="this.$slots.default[0]"></div>
+      </div>
+      <span class="line" ref="line"></span>
+      <span v-if="closeButton" class="close" @click="onCloseButton">{{closeButton.text}}</span>
     </div>
-    <span class="line" ref="line"></span>
-    <span v-if="closeButton" class="close" @click="onCloseButton">{{closeButton.text}}</span>
   </div>
 </template>
 <script>
@@ -51,7 +53,7 @@ export default {
     updateLineStyle() {
       this.$nextTick(() => {
         this.$refs.line.style.height =
-          this.$refs.wapper.getBoundingClientRect().height + "px";
+          this.$refs.toast.getBoundingClientRect().height + "px";
       });
     },
     exclAutoClose() {
@@ -63,7 +65,7 @@ export default {
     },
     close() {
       this.$el.remove();
-      this.$emit('close')
+      this.$emit("close");
       this.$destroy();
     },
     onCloseButton() {
@@ -76,44 +78,76 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.toast {
+.wapper {
   position: fixed;
   left: 50%;
 
-  transform: translateX(-50%);
-  min-height: 40px;
-  display: flex;
-  align-items: center;
-  padding: 0 16px;
-  background: #a94442;
-  color: #fff;
-
-  font-size: 14px;
   &.align-top {
     top: 0;
     border-radius: 0 0 4px 4px;
+    animation: slide-in 0.3s linear;
   }
   &.align-middle {
     top: 50%;
-    transform: translateX(-50%) translateY(-50%);
+    transform: translateY(-50%);
     border-radius: 4px;
+    animation: fade-in 0.3s linear;
   }
   &.align-bottom {
     bottom: 0;
     border-radius: 4px 4px 0 0;
+    animation: slide-up 0.3s linear;
   }
-  & > .message {
-    padding: 8px 0;
+  @keyframes slide-up {
+    0% {
+      transform: translateY(100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0%);
+      opacity: 1;
+    }
   }
-  & > .line {
-    border-left: solid white 2px;
-    display: block;
-    height: 100%;
-    margin: 0 10px;
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
   }
-  & > .close {
-    flex-shrink: 0;
-    cursor: pointer;
+  @keyframes slide-in {
+    0% {
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0%);
+      opacity: 1;
+    }
+  }
+  .toast {
+    transform: translateX(-50%);
+    min-height: 40px;
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+    background: #a94442;
+    color: #fff;
+    font-size: 14px;
+    & > .message {
+      padding: 8px 0;
+    }
+    & > .line {
+      border-left: solid white 2px;
+      display: block;
+      height: 100%;
+      margin: 0 10px;
+    }
+    & > .close {
+      flex-shrink: 0;
+      cursor: pointer;
+    }
   }
 }
 </style>
