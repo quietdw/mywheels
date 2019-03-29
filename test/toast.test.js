d53b22813f6a8fb35b5a8be92f6eaeae987e79b5
quietdw <1044569102@qq.com>
2019-03-29 16:48:43 +0800

    toast测试用例

---
+++ test/toast.test.js
@@ -0,0 +1,72 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Toast from '../src/toast'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Toast', () => {
+  it('存在Toast', () => {
+    expect(Toast).to.be.ok
+  })
+
+  describe('props', () => {
+    const Constructor = Vue.extend(Toast)
+    let vm
+    afterEach(function () {
+      vm.$destroy()
+    })
+    it('autoClose', (done) => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      vm = new Constructor({
+        propsData: {
+          autoClose: 1
+        }
+      }).$mount(div)
+      vm.$on('close', () => {
+        expect(div.contains(vm.$el)).to.eq(false)
+        done()
+      })
+    })
+    it('closeButton', () => {
+      const callback = sinon.fake()
+      vm = new Constructor({
+        propsData: {
+          closeButton: {
+            text: "关闭123",
+            callback
+          }
+        }
+      }).$mount()
+      let colseButton = vm.$el.querySelector('.close')
+      expect(colseButton.textContent.trim()).to.eq('关闭123')
+      colseButton.click()
+      expect(callback).to.have.been.called
+    })
+    it('enableHTML', () => {
+      vm = new Constructor({
+        propsData: {
+          enableHTML: true
+        }
+      })
+
+      vm.$slots.default = ['<strong id ="hihi">111</strong>']
+      vm.$mount()
+      let messageStrong = vm.$el.querySelector('#hihi')
+      expect(messageStrong.textContent.trim()).to.eq('111')
+    })
+    it('align', () => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      vm = new Constructor({
+        propsData: {
+          align: 'middle'
+        }
+      }).$mount(div)
+      expect(vm.$el.classList.contains('align-middle')).to.eq(true)
+    })
+
+  })
+
+})
