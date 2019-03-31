c717823bcb6a981c5f6b3d667e90ca8069c4cb95
quietdw <1044569102@qq.com>
2019-03-31 16:52:37 +0800

    防止 父元素overflow导致popover被挡

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click="xxx">
-    <div class="content-wapper" v-if="visible" @click.stop>
+    <div class="content-wapper" v-if="visible" ref="contentWapper">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span class="trigger-wrapper" ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -15,11 +17,22 @@ export default {
       visible: false
     };
   },
+  mounted() {},
   methods: {
     xxx() {
       this.visible = !this.visible;
       setTimeout(() => {
         if (this.visible) {
+          document.body.appendChild(this.$refs.contentWapper);
+          let {
+            width,
+            height,
+            left,
+            top
+          } = this.$refs.triggerWrapper.getBoundingClientRect();
+          let content = this.$refs.contentWapper;
+          content.style.left = `${left}px`;
+          content.style.top = `${top}px`;
           document.addEventListener("click", eventHandler);
         }
       }, 0);
@@ -37,11 +50,15 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  margin: 100px;
+  margin: 100px 0 0 100px;
   .content-wapper {
     position: absolute;
     bottom: 100%;
     left: 0;
   }
+}
+.content-wapper {
+  position: absolute;
+  transform: translateY(-100%);
 }
 </style>
