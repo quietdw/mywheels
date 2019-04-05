e381942f6c08d78f99b60df15e3d3642dc1066de
quietdw <1044569102@qq.com>
2019-04-06 00:00:16 +0800

    在md文件中使用组件

---
+++ docs/.vuepress/components/button-demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <g-button>默认按钮</g-button>
+    <g-button>默认按钮</g-button>
+    <g-button>
+      <g-icon name="settings"></g-icon>有图标的
+    </g-button>
+  </div>
+</template>
+
+<script>
+import Button from "../../../src/button";
+import Icon from "../../../src/icon";
+export default {
+  components: { "g-button": Button, "g-icon": Icon }
+};
+</script>
--- docs/components/button.md
+++ docs/components/button.md
@@ -4,4 +4,6 @@ title:按钮
 
 # 按钮
 
-  使用方法
+使用方法
+
+<button-demo></button-demo>
