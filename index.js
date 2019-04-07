0476c80d798fc1796709cc06bb0dc0bf7f12fd31
quietdw <1044569102@qq.com>
2019-04-07 19:55:15 +0800

    npm包 index.js

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 import Button from './src/button'
 import Icon from './src/icon'
 import ButtonGroup from './src/button-group'
+import Col from './src/col'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapse-item'
+import Content from './src/content'
+import Footer from './src/footer'
+import Header from './src/header'
+import Input from './src/input'
+import Layout from './src/layout'
+import Popover from './src/popover'
+import Row from './src/row'
+import Sider from './src/sider'
+import Toast from './src/toast'
+import Tabs from './src/tabs/tabs'
+import TabsBody from './src/tabs/tabs-body'
+import TabsHead from './src/tabs/tabs-head'
+import TabsItem from './src/tabs/tabs-item'
+import TabsPane from './src/tabs/tabs-pane'
 
-export {Button,ButtonGroup,Icon}
+import plugin from './src/plugin'
+
+export {
+  Button,
+  ButtonGroup,
+  Icon,
+  Col,
+  Collapse,
+  CollapseItem,
+  Content,
+  Footer,
+  Header,
+  Input,
+  Layout,
+  Popover,
+  Row,
+  Sider,
+  Toast,
+  Tabs,
+  TabsBody,
+  TabsHead,
+  TabsItem,
+  TabsPane,
+  plugin
+}
